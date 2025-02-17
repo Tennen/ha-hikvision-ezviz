0554484ff7d267d6848fba20c0d3f5cc58cd9e70
Tennen <cell1201@gmail.com>
2025-02-17 16:59:19 +0800

    try modify login

--- custom_components/hikvision_enviz/hikvision_api.py
+++ custom_components/hikvision_enviz/hikvision_api.py
@@ -405,15 +405,18 @@ class HikvisionEnvizAPI:
             
             # Convert string inputs to bytes for SDK
             login_info.sDeviceAddress = self._host.encode()
-            login_info.byLoginMode = 0
             login_info.bUseAsynLogin = 0
             login_info.wPort = self._port
             login_info.sUserName = self._username.encode()
             login_info.sPassword = self._password.encode()
-            # struDeviceInfoV40 = NET_DVR_DEVICEINFO_V40()
+            login_info.byLoginMode = 1    # ISAPI模式
+            login_info.byHttps = 0        # 禁用HTTPS
+            login_info.byVerifyMode = 0   # 禁用验证
+
+            struDeviceInfoV40 = NET_DVR_DEVICEINFO_V40()
             
             # Try to login
-            user_id = self._hik_sdk.NET_DVR_Login_V30(self._host.encode(), self._port, self._username.encode(), self._password.encode())
+            user_id = self._hik_sdk.NET_DVR_Login_V40(byref(login_info), byref(struDeviceInfoV40))
             
             if user_id < 0:
                 error_code = self._hik_sdk.NET_DVR_GetLastError()
