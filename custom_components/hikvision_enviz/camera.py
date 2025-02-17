512047b74d06ab05d0dbf9cf51a13b8a6881415f
Tennen <cell1201@gmail.com>
2025-02-18 00:05:12 +0800

    try

--- custom_components/hikvision_enviz/camera.py
+++ custom_components/hikvision_enviz/camera.py
@@ -9,7 +9,6 @@ import io
 from homeassistant.components.camera import (
     Camera,
     CameraEntityFeature,
-    SUPPORT_STREAM,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -19,44 +18,40 @@ from homeassistant.helpers import entity_platform
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 
-from .const import DOMAIN
+from . import DOMAIN
 from .hikvision_api import HikvisionEnvizAPI
 
 _LOGGER = logging.getLogger(__name__)
-
-# 定义服务的名称
-SERVICE_PTZ = "ptz_control"
-
-# 定义服务的参数模式
-PTZ_SCHEMA = vol.Schema({
-    vol.Optional("pan", default=0): vol.All(
-        vol.Coerce(float), vol.Range(min=-1, max=1)
-    ),
-    vol.Optional("tilt", default=0): vol.All(
-        vol.Coerce(float), vol.Range(min=-1, max=1)
-    ),
-    vol.Optional("zoom", default=0): vol.All(
-        vol.Coerce(float), vol.Range(min=-1, max=1)
-    ),
-})
 
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
-) -> None:
-    """Set up Hikvision Enviz Camera from config entry."""
+) -> bool:
+    """Set up Hikvision camera from a config entry."""
     api = hass.data[DOMAIN][entry.entry_id]
     
     # 注册 PTZ 服务
     platform = entity_platform.async_get_current_platform()
     platform.async_register_entity_service(
-        SERVICE_PTZ,
-        PTZ_SCHEMA,
+        "ptz_control",
+        {
+            vol.Optional("pan", default=0): vol.All(
+                vol.Coerce(float), vol.Range(min=-1, max=1)
+            ),
+            vol.Optional("tilt", default=0): vol.All(
+                vol.Coerce(float), vol.Range(min=-1, max=1)
+            ),
+            vol.Optional("zoom", default=0): vol.All(
+                vol.Coerce(float), vol.Range(min=-1, max=1)
+            ),
+        },
         "ptz_control"
     )
     
-    async_add_entities([HikvisionEnvizCamera(api, entry)], True)
+    camera = HikvisionEnvizCamera(api, entry)
+    async_add_entities([camera], True)
+    return True
 
 class HikvisionEnvizCamera(Camera):
     """Representation of a Hikvision Enviz Camera."""
@@ -68,43 +63,50 @@ class HikvisionEnvizCamera(Camera):
         self._entry = entry
         self._attr_name = entry.title
         self._attr_unique_id = entry.entry_id
-        self._attr_supported_features = (
-            CameraEntityFeature.STREAM
-        )
-        self._frame_interval = 1/30  # 30 FPS
-        self._current_image = None
+        self._attr_supported_features = CameraEntityFeature.ON_OFF | CameraEntityFeature.STREAM
+        self._stream_handler = None
 
-    async def _handle_frame(self, frame_data: bytes) -> None:
-        """Handle incoming frame data."""
-        try:
-            # Convert frame data to image
-            image = Image.open(io.BytesIO(frame_data))
-            # Save as JPEG
-            img_byte_arr = io.BytesIO()
-            image.save(img_byte_arr, format='JPEG')
-            self._current_image = img_byte_arr.getvalue()
-        except Exception as e:
-            _LOGGER.error("Error processing frame: %s", str(e))
+    @property
+    def supported_features(self) -> int:
+        """Return supported features."""
+        return self._attr_supported_features
 
     async def async_camera_image(
         self, width: int | None = None, height: int | None = None
     ) -> bytes | None:
-        """Return a still image from the camera."""
-        return self._current_image
+        """Return a still image response from the camera."""
+        try:
+            return await self.hass.async_add_executor_job(self._api.get_snapshot)
+        except Exception as e:
+            _LOGGER.error("Error getting camera image: %s", str(e))
+            return None
 
-    async def async_added_to_hass(self) -> None:
-        """Handle entity addition to hass."""
-        await self._api.connect()
-        # Start streaming when added to hass
-        await self._api.start_stream(self._handle_frame)
+    async def handle_async_mjpeg_stream(self, request):
+        """Return an MJPEG stream."""
+        if not self._stream_handler:
+            self._stream_handler = await self.hass.async_add_executor_job(
+                self._api.start_stream
+            )
+        return await self._stream_handler(request)
 
-    async def async_will_remove_from_hass(self) -> None:
-        """Handle entity removal from hass."""
-        await self._api.stop_stream()
-        await self._api.disconnect()
+    async def async_turn_off(self):
+        """Turn off camera."""
+        if self._stream_handler:
+            await self.hass.async_add_executor_job(self._api.stop_stream)
+            self._stream_handler = None
+
+    async def async_turn_on(self):
+        """Turn on camera."""
+        if not self._stream_handler:
+            self._stream_handler = await self.hass.async_add_executor_job(
+                self._api.start_stream
+            )
 
     async def ptz_control(self, pan=0, tilt=0, zoom=0):
         """Handle PTZ service call."""
-        await self.hass.async_add_executor_job(
-            self._api.ptz_control, pan, tilt, zoom
-        ) 
+        try:
+            await self.hass.async_add_executor_job(
+                self._api.ptz_control, pan, tilt, zoom
+            )
+        except Exception as e:
+            _LOGGER.error("Error controlling PTZ: %s", str(e)) 
